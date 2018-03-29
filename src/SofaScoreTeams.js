dc0f044f7dc76085ec5e11aa3dd197b50475487a
João Ferreira <joao.ferreira@wig.pt>
2018-03-29 12:25:43 +0100

    testing

--- src/SofaScoreTeams.js
+++ src/SofaScoreTeams.js
@@ -3,9 +3,30 @@ var vo = require('vo');
 
 function* Job() {
     console.log('hello job')
-    var results = yield teamsJob.scrapGames(['https://www.whoscored.com/Teams/32/Show/England-Manchester-United',
-    ' https://www.whoscored.com/Teams/162/Show/England-Crystal-Palace',
-    'https://www.whoscored.com/Teams/15/Show/England-Chelsea'])
+    var results = yield teamsJob.scrapGames(
+        [
+           {
+               nextGame:{
+                   homeTeamName : 'MU',
+                   awayTeamName : 'CH',
+                   homeTeamLink : 'https://www.whoscored.com/Teams/32/Show/England-Manchester-United'
+               }
+           } ,
+           {
+            nextGame:{
+                homeTeamName : 'MU',
+                awayTeamName : 'CH',
+                homeTeamLink : 'https://www.whoscored.com/Teams/162/Show/England-Crystal-Palace'
+            }
+        } ,
+        {
+            nextGame:{
+                homeTeamName : 'MU',
+                awayTeamName : 'CH',
+                homeTeamLink : 'https://www.whoscored.com/Teams/15/Show/England-Chelsea'
+            }
+        }
+])
 
 }
 
